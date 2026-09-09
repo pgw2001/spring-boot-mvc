f170e8039d58ec39141b1a0ad9e6445b2a5815f6
pgw2001 <pgy2008@naver.com>
2026-09-09 11:37:14 +0900

    Initial commit

---
+++ src/test/java/com/kimyanu/blog/BlogMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimyanu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
